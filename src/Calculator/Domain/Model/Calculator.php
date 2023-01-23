21f6fd5b13b1a7e0928c71666106fa75f6572e5e
AlexandruNicolaePopa <120518017+AlexandruNicolaePopa@users.noreply.github.com>
2023-01-23 14:30:44 +0200

    Update Calculator.php
    Added verification to make sure we do not pass something else to the eval method.

--- src/Calculator/Domain/Model/Calculator.php
+++ src/Calculator/Domain/Model/Calculator.php
@@ -20,6 +20,30 @@ class Calculator
         if (strpos($expression, 'sqrt') !== false) {
             $expression = preg_replace('/sqrt/', 'sqrt', $expression);
         }
+
+        // check if the expression contains invalid characters
+        if (!preg_match('/^[\d\s+\-\*\/\.]+$/', $expression)) {
+            throw new \InvalidArgumentException('Invalid expression.');
+        }
+
+        // check if the expression starts or ends with an operator
+        if (preg_match('/^[\+\-\*\/]|[\+\-\*\/]$/', $expression)) {
+            throw new \InvalidArgumentException('Invalid expression.');
+        }
+
+        // check if the expression contains consecutive operators
+        if (preg_match('/[\+\-\*\/]{2,}/', $expression)) {
+            throw new \InvalidArgumentException('Invalid expression.');
+        }
+
+        // Replace " * " and " / " with their corresponding mathematical functions - stripping spaces
+        $expression = preg_replace('/\*/', '*', $expression);
+        $expression = preg_replace('/\//', '/', $expression);
+
+        // Replace " + " and " - " with their corresponding mathematical functions - stripping spaces
+        $expression = preg_replace('/\+/', '+', $expression);
+        $expression = preg_replace('/\-/', '-', $expression);
+
         try {
             return eval('return ' . $expression . ';');
         } catch (Error $e) {
